0e7a48fe4a45c35e175a24a213f30eb1f3cc793f
Ming-der Wang <mingderwang@gmail.com>
2022-04-02 20:48:19 +0800

    change to use daisyUI, button, input

--- components/SiteLayout.js
+++ components/SiteLayout.js
@@ -19,8 +19,9 @@ const SiteLayout = ({ children }) => (
             </div>
             <div className="mt-2">
               <input
-                className="block w-full border border-gray-300 rounded-lg bg-gray-100 px-3 py-2 leading-tight focus:outline-none focus:border-gray-600 focus:bg-white"
+                type="text"
                 placeholder="Search..."
+                class="input input-bordered input-warning w-full"
               />
             </div>
           </nav>
@@ -31,6 +32,6 @@ const SiteLayout = ({ children }) => (
   </div>
 );
 
-export const getLayout = page => <SiteLayout>{page}</SiteLayout>;
+export const getLayout = (page) => <SiteLayout>{page}</SiteLayout>;
 
 export default SiteLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Link from 'next/link'
-import { getLayout } from '../components/SiteLayout'
+import Link from "next/link";
+import { getLayout } from "../components/SiteLayout";
 
 const Index = () => (
-  <div className="mt-8 max-w-xl mx-auto px-8">
+  <div className="flex-container">
     <h1 className="text-center">
       <span className="block text-xl text-gray-600 leading-tight">
         Welcome to this
@@ -13,14 +13,12 @@ const Index = () => (
     </h1>
     <div className="mt-12 text-center">
       <Link href="/account-settings/basic-information">
-        <a className="inline-block bg-gray-900 hover:bg-gray-800 text-white font-medium rounded-lg px-6 py-4 leading-tight">
-          View account settings
-        </a>
+        <a className="btn">View account settings</a>
       </Link>
     </div>
   </div>
-)
+);
 
-Index.getLayout = getLayout
+Index.getLayout = getLayout;
 
-export default Index
+export default Index;
