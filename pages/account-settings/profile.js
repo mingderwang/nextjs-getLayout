82e01241e89d9d31f9bbfaef217d2d85d1aa31ff
Ming-der Wang <mingderwang@gmail.com>
2022-04-02 21:27:01 +0800

    cancel save buttons use daisyUI

--- pages/account-settings/profile.js
+++ pages/account-settings/profile.js
@@ -24,18 +24,14 @@ const AccountSettingsProfile = () => (
       </div>
     </div>
     <div className="border-t-2 border-gray-200 px-0 py-5 flex justify-end">
-      <button
-        type="button"
-        className="px-4 py-3 leading-none font-semibold rounded-lg bg-gray-300 text-gray-900 hover:bg-gray-400 focus:outline-none focus:bg-gray-400"
-      >
+      <button type="button" className="btn btn-ghost">
         Cancel
       </button>
-      <button
-        type="button"
-        className="ml-4 px-6 py-3 leading-none font-semibold rounded-lg bg-gray-800 text-white hover:bg-gray-900 focus:outline-none focus:bg-gray-900"
-      >
-        Save
-      </button>
+      <div className="px-2">
+        <button type="button" className="btn btn-secondary">
+          Save
+        </button>
+      </div>
     </div>
   </div>
 );
